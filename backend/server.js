6ef62223ec43ab9230f115f3d6e3d5f429a51833
Mason Nguyen <masonnguyen18@gmail.com>
2021-03-03 17:58:48 -0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import users from './data/users.js'
+
+import connectDB from './config/db.js'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 8000
+
+connectDB()
+
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.send(`Server is running on port ${PORT}`)
+})
+
+app.get('/users', (req, res)=>{
+    res.json(users)
+})
+
+app.get('/users/:id', (req, res)=>{
+    const user = users.find((u) => u._id === req.params.id)
+    res.json(user)
+})
+
+
+app.listen(PORT, console.log('The server has started'))
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
 
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import {Container, Row, Col} from 'react-bootstrap'
+import Header from './components/Header.js'
+import Footer from './components/Footer.js'
+import HomeScreen from './screens/HomeScreen.js'
+import UserScreen from './screens/UserScreen.js'
+
 
 const App = () => {
+ 
   return (
-    <div className="App">
-      <h1>Welcome To TutorPlace</h1>
-    </div>
+    <Router>
+      <Header></Header>
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact></Route>
+          <Route path='/users/:id' component={UserScreen}></Route>
+        </Container>
+        
+      </main>
+      <Footer></Footer>
+    </Router>
   );
 }
 
