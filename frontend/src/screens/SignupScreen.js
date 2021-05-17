062935f27b5c5fae05071eefc337cf1ad9f092e4
Mason Ng <masonnguyen18@gmail.com>
2021-05-16 22:09:18 -0700

    fix bugs

--- frontend/src/screens/SignupScreen.js
+++ frontend/src/screens/SignupScreen.js
@@ -1,74 +1,73 @@
 import React, { useState, useEffect } from 'react'
-import  { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Form, Button, Row, Col } from 'react-bootstrap'
-import SignInContainer from '../components/SignInContainer'
-import {register} from '../actions/userActions'
+import FormContainer from '../components/FormContainer'
+import { register } from '../actions/userActions'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 
-const SignUpScreen = ({ location, history}) => {
+const SignUpScreen = ({ location, history }) => {
   const [firstname, setFirstName] = useState('')
   const [lastname, setLastName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
-  const[message, setMessage] = useState(null)
+  const [message, setMessage] = useState(null)
   const redirect = location.search ? location.search.split('=')[1] : '/'
   const dispatch = useDispatch()
 
   const userRegister = useSelector((state) => state.userRegister)
   const { loading, error, Info_user } = userRegister
 
-  useEffect(()=>{
-    if(Info_user)
-    {
+  useEffect(() => {
+    if (Info_user) {
       history.push(redirect)
     }
-  },[history, Info_user, redirect])
-  
+  }, [history, Info_user, redirect])
+
   const submitHandler = (e) => {
     e.preventDefault()
-    if(password!==confirmPassword)
+    if (password !== confirmPassword)
       setMessage('Password does not match ')
     else
-      dispatch(register(firstname,lastname, email, password))
-  
-  
-  } 
-  
+      dispatch(register(firstname, lastname, email, password))
+
+
+  }
+
 
   return (
-    <SignInContainer>
+    <FormContainer>
       <h1>Create your TutorPlace Account</h1>
       {message && <Message variant='danger'>{message}</Message>}
       {error && <Message variant='danger'>{error}</Message>}
-      {loading && <Loader/>}
+      {loading && <Loader />}
       <Form onSubmit={submitHandler}>
         <Form.Row>
-        <Col>
-        <Form.Group controlId='firstname'>
-          <Form.Label>First Name</Form.Label>
-          <Form.Control
-            type='firstname'
-            placeholder='First Name'
-            value={firstname}
-            onChange={(e) => setFirstName(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        </Col>
-        <Col>
-        <Form.Group controlId='lastname'>
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control
-            type='lastname'
-            placeholder='Last Name'
-            value={lastname}
-            onChange={(e) => setLastName(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        </Col>
-       </Form.Row>
+          <Col>
+            <Form.Group controlId='firstname'>
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                type='firstname'
+                placeholder='First Name'
+                value={firstname}
+                onChange={(e) => setFirstName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId='lastname'>
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                type='lastname'
+                placeholder='Last Name'
+                value={lastname}
+                onChange={(e) => setLastName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+          </Col>
+        </Form.Row>
         <Form.Group controlId='email'>
           <Form.Label>Email Address</Form.Label>
           <Form.Control
@@ -112,7 +111,7 @@ const SignUpScreen = ({ location, history}) => {
           </Link>
         </Col>
       </Row>
-    </SignInContainer>
+    </FormContainer>
   )
 }
 
