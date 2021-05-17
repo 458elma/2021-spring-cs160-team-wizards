1de1b790819fdd324ec7315e507df5262d2547ff
Mason Ng <masonnguyen18@gmail.com>
2021-05-16 21:34:04 -0700

    fix bugs

--- frontend/src/screens/SignupScreen.js
+++ frontend/src/screens/SignupScreen.js
@@ -1,63 +1,79 @@
 import React, { useState, useEffect } from 'react'
+import  { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Form, Button, Row, Col } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
+import SignInContainer from '../components/SignInContainer'
+import {register} from '../actions/userActions'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import FormContainer from '../components/FormContainer'
-import { register } from '../actions/userActions'
 
-const RegisterScreen = ({ location, history }) => {
-  const [name, setName] = useState('')
+const SignUpScreen = ({ location, history}) => {
+  const [firstname, setFirstName] = useState('')
+  const [lastname, setLastName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
-  const [message, setMessage] = useState(null)
-
+  const[message, setMessage] = useState(null)
+  const redirect = location.search ? location.search.split('=')[1] : '/'
   const dispatch = useDispatch()
 
   const userRegister = useSelector((state) => state.userRegister)
-  const { loading, error, userInfo } = userRegister
+  const { loading, error, Info_user } = userRegister
 
-  const redirect = location.search ? location.search.split('=')[1] : '/'
-
-  useEffect(() => {
-    if (userInfo) {
+  useEffect(()=>{
+    if(Info_user)
+    {
       history.push(redirect)
     }
-  }, [history, userInfo, redirect])
-
+  },[history, Info_user, redirect])
+  
   const submitHandler = (e) => {
     e.preventDefault()
-    if (password !== confirmPassword) {
-      setMessage('Passwords do not match')
-    } else {
-      dispatch(register(name, email, password))
-    }
-  }
+    if(password!==confirmPassword)
+      setMessage('Password does not match ')
+    else
+      dispatch(register(firstname,lastname, email, password))
+  
+  
+  } 
+  
 
   return (
-    <FormContainer>
-      <h1>Sign Up</h1>
+    <SignInContainer>
+      <h1>Create your TutorPlace Account</h1>
       {message && <Message variant='danger'>{message}</Message>}
       {error && <Message variant='danger'>{error}</Message>}
-      {loading && <Loader />}
+      {loading && <Loader/>}
       <Form onSubmit={submitHandler}>
-        <Form.Group controlId='name'>
-          <Form.Label>Name</Form.Label>
+        <Form.Row>
+        <Col>
+        <Form.Group controlId='firstname'>
+          <Form.Label>First Name</Form.Label>
           <Form.Control
-            type='name'
-            placeholder='Enter name'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            type='firstname'
+            placeholder='First Name'
+            value={firstname}
+            onChange={(e) => setFirstName(e.target.value)}
           ></Form.Control>
         </Form.Group>
-
+        </Col>
+        <Col>
+        <Form.Group controlId='lastname'>
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control
+            type='lastname'
+            placeholder='Last Name'
+            value={lastname}
+            onChange={(e) => setLastName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        </Col>
+       </Form.Row>
         <Form.Group controlId='email'>
           <Form.Label>Email Address</Form.Label>
           <Form.Control
             type='email'
-            placeholder='Enter email'
+            placeholder='Enter Email'
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           ></Form.Control>
@@ -67,7 +83,7 @@ const RegisterScreen = ({ location, history }) => {
           <Form.Label>Password</Form.Label>
           <Form.Control
             type='password'
-            placeholder='Enter password'
+            placeholder='Enter Password'
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
@@ -77,14 +93,14 @@ const RegisterScreen = ({ location, history }) => {
           <Form.Label>Confirm Password</Form.Label>
           <Form.Control
             type='password'
-            placeholder='Confirm password'
+            placeholder='Confirm Password'
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Button type='submit' variant='primary'>
-          Register
+        <Button type='submit' variant='primary' size="lg" block>
+          Sign Up
         </Button>
       </Form>
 
@@ -92,12 +108,12 @@ const RegisterScreen = ({ location, history }) => {
         <Col>
           Have an Account?{' '}
           <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>
-            Login
+            Sign in instead
           </Link>
         </Col>
       </Row>
-    </FormContainer>
+    </SignInContainer>
   )
 }
 
-export default RegisterScreen
+export default SignUpScreen
